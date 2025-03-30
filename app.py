3d9b0d51d9e040b18f8460844a142ca38555ed76
Ignacio Eleazar Marceca <imarceca@gmail.com>
2025-03-30 16:44:00 +0200

    BQAPY-2
    Grade prompt included, now we can see if its PASS, FAIL etc

--- app.py
+++ app.py
@@ -14,8 +14,25 @@ def calculate_grade():
     grade3 = float(request.form["grade3"])
 
     final_grade = (grade1 + grade2 + grade3) / 3
+    grade_prompt = "SIN NOTA"
+    if (final_grade >= 5):
+        grade_prompt = "APROBADO"
+    elif (final_grade > 5 & final_grade <= 9 ):
+        grade_prompt = "NOTABLE"
+    else:
+        grade_prompt = "SOBRESALIENTE"
 
-    return render_template("result.html", name=name, final_grade=round(final_grade, 2))
+    return render_template("result.html", name=name, final_grade=round(final_grade, 2), grade_prompt= grade_prompt)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+def notaFinal(nota):
+    if (nota > 8):
+        mensaje = "SOBRESALIENTE"
+    elif (nota < 5):
+        mensaje = "SUSPENSO"
+    else:
+        mensaje = "APROBADO"
+    return mensaje
